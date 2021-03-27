bc6e3c23bc30edeab85b55734857a68864f735aa
Osakinle Thomas <Thomasdreh@gmail.com>
2021-03-27 15:36:51 +0800

    AuthenticationDBContext, which helps in registering users is created within the 'Entities' folder that would house all user classes. Inside this 'Entities Folder' resides the file 'ApplicationUser' which is a class that controls registration of user

--- src/RealEstateApp.Data/DatabaseContext/AuthenticationDbContext/AuthenticationDbContext.cs
+++ src/RealEstateApp.Data/DatabaseContext/AuthenticationDbContext/AuthenticationDbContext.cs
@@ -1,7 +1,14 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RealEstateApp.Data.Entities;
+
 namespace RealEstateApp.Data.DatabaseContext.AuthenticationDbContext
 {
-    public class AuthenticationDbContext: IdentityDbContext
+    public class AuthenticationDbContext: IdentityDbContext <ApplicationUser>
+    {
+    public AuthenticationDbContext(DbContextOptions<AuthenticationDbContext>options): base(options)
     {
 
+    }
     }
 }
---
+++ src/RealEstateApp.Data/Entities/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace RealEstateApp.Data.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        
+    }
+}
