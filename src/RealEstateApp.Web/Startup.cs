14f978307409c8f78edbf0d4b082a429c82888a1
Osakinle Thomas <Thomasdreh@gmail.com>
2021-04-01 13:57:01 -0400

    Database connection string added

--- src/RealEstateApp.Web/Startup.cs
+++ src/RealEstateApp.Web/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RealEstateApp.Data.DatabaseContext.AuthenticationDbContext;
 
 namespace RealEstateApp.Web
 {
@@ -23,6 +25,8 @@ namespace RealEstateApp.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContextPool<AuthenticationDbContext>(options => 
+            options.UseSqlServer(Configuration.GetConnectionString("AuthenticationConnection")));
             services.AddControllersWithViews();
         }
 
