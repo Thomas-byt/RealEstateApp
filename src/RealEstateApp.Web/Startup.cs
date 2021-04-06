af55e9aa41d72a1b8e5b9174f7a28ef034bfcd82
Osakinle Thomas <Thomasdreh@gmail.com>
2021-04-07 03:46:42 +0800

    created default users and roles

--- src/RealEstateApp.Web/Startup.cs
+++ src/RealEstateApp.Web/Startup.cs
@@ -5,12 +5,14 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using RealEstateApp.Data.DatabaseContext.ApplicationDbContext;
 using RealEstateApp.Data.DatabaseContext.AuthenticationDbContext;
+using RealEstateApp.Data.Entities;
 
 namespace RealEstateApp.Web
 {
@@ -34,12 +36,28 @@ namespace RealEstateApp.Web
                 sqlServerOptions => {
                 sqlServerOptions.MigrationsAssembly("RealEstateApp.Data");
             }));
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+            .AddEntityFrameworkStores<AuthenticationDbContext>()
+            .AddDefaultTokenProviders();
+
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 6;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.SignIn.RequireConfirmedEmail = false;
+                options.SignIn.RequireConfirmedPhoneNumber = false;
+
+            });
 
             services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider svp)
         {
             if (env.IsDevelopment())
             {
@@ -64,6 +82,51 @@ namespace RealEstateApp.Web
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+            MigrateDatabaseContexts(svp);
+            CreateDefaultRolesAndUsers(svp).GetAwaiter().GetResult();
+        }
+        public void MigrateDatabaseContexts(IServiceProvider svp)
+        {
+            var AuthenticationDbContext =svp.GetRequiredService<AuthenticationDbContext>();
+            AuthenticationDbContext.Database.Migrate();
+
+            var ApplicationDbContext = svp.GetRequiredService<ApplicationDbContext>();
+            ApplicationDbContext.Database.Migrate();
+        }
+        public async Task CreateDefaultRolesAndUsers(IServiceProvider svp)
+        {
+            string[] roles = new string [] {"SystemAdministrator", "Agent", "User", };
+            var userEmail = "admin@RealEstateApp.com";
+            var userPassword = "SupersecretPassword@2020";
+
+            var roleManager = svp.GetRequiredService<RoleManager<IdentityRole>>();
+            foreach (var role in roles)
+            {
+                var roleExists = await roleManager.RoleExistsAsync(role);
+                if(!roleExists)
+                {
+                    await roleManager.CreateAsync(new IdentityRole{Name = role});
+                }
+            }
+
+            var userManager = svp.GetRequiredService<UserManager<ApplicationUser>>();
+
+            var user = await userManager.FindByEmailAsync(userEmail);
+            if( user is null)
+            {
+            user = new ApplicationUser
+                {
+                    Email = userEmail,
+                    UserName = userEmail,
+                    EmailConfirmed = true,
+                    PhoneNumber = "2348060815202",
+                    PhoneNumberConfirmed = true
+
+                };
+                await userManager.CreateAsync(user, userPassword);
+                await userManager.AddToRolesAsync(user, roles);
+            }
+            
         }
     }
 }
